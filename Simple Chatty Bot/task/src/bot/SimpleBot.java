8a12ba99500157db6f6ae8dd101abbde10148615
Omar Taher <omartaher@Omars-MacBook-Air.local>
2022-09-27 13:44:26 +0300

    Stage 4/5

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -12,11 +12,17 @@ public class SimpleBot {
         System.out.println("What a great name you have, " + name + "!");
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
-        // reading all remainders
-        int r3 = scanner.nextInt();
-        int r5 = scanner.nextInt();
-        int r7 = scanner.nextInt();
-        int age = (r3 * 70 + r5 * 21 + r7 * 15) % 105;
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        // read a number and count to it here
+        int n = scanner.nextInt();
+        for (int i = 0; i <= n; i++) {
+            System.out.println(i + "!");
+        }
+        System.out.println("Completed, have a nice day!");
     }
 }
