8cd962675e5bfb8050da268313529557e5eddbe0
Omar Taher <omartaher@Omars-MacBook-Air.local>
2022-09-27 13:39:31 +0300

    Stage 2/5

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,9 +1,15 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
-        // write your code here
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! My name is Chatty.");
         System.out.println("I was created in 2022.");
+        System.out.println("Please, remind me your name.");
+        // reading a name
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
     }
 }
